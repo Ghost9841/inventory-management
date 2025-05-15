b639c833499d6faeff41685c8505d07e74fc8481
Ghost9841 <asubedi778@gmail.com>
2025-05-15 19:45:50 +0545

    Now will work on Navbar and will probably work on side bar

--- client/src/app/dashboard/DashboardWrapper.tsx
+++ client/src/app/dashboard/DashboardWrapper.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className={`dark flex bg-gray-200 w-full min-h-screen`}>
+    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
         Sidebar
-        <main className="dark flex flex-col w-full h-full py-7 px-9 bg-gray-200">
+        <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50
+        md:pl-24 ">
+          
         {children}
         </main>
     </div>
