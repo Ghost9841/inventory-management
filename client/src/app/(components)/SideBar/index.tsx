504b230f922f2b4d6a1d3dc861f2b2d26a358069
Ghost9841 <asubedi778@gmail.com>
2025-05-16 18:35:05 +0545

    Well Layout is done ig for sidebar and I am installing Redux ToolKit now, Well I am going to use a BoilerPlate which can be used in every NEXTJS App

--- client/src/app/(components)/SideBar/index.tsx
+++ client/src/app/(components)/SideBar/index.tsx
@@ -1,10 +1,35 @@
+"use client"
+
+import { Menu } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <section id="sidebar">
+        {/* Top Logo */}
+        <div  className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+            <img src="" alt="logo" className="" />
+            <h1 className="font-extrabold text-2xl">Tech Nest</h1>
+        
+        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={()=> {}}>
+            <Menu className='w-4 h-4'/>
+        </button>
+        </div>
+
+        {/* Links */}
+        <div className="flex-grow mt-8">
+            {/* Links */}
+        </div>
+
+        {/* Footer */}
+        <div className="">
+            <p className="text-center text-xs text-gray-500">&copy; 2025 Tech Nest</p>
+
+        </div>
+    </section>
   )
 }
 
