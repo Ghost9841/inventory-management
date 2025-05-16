b896b5e94e2e95ad664bab8853fa009453798a59
Ghost9841 <asubedi778@gmail.com>
2025-05-16 17:52:31 +0545

    Navbar done, Now about to do sidebar

--- client/src/app/dashboard/DashboardWrapper.tsx
+++ client/src/app/dashboard/DashboardWrapper.tsx
@@ -1,13 +1,16 @@
+
 import React from 'react'
+import NavBar from '../(components)/NavBar';
+import SideBar from '../(components)/SideBar';
 
 
 const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-        Sidebar
+       <SideBar/>
         <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50
         md:pl-24 ">
-          
+          <NavBar/>
         {children}
         </main>
     </div>
