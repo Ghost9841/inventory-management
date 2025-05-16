c39e7da174937736578c485a05f82d1fb1cd1aaa
Ghost9841 <asubedi778@gmail.com>
2025-05-16 19:42:24 +0545

    Added BoilerPlate of Redux.tsx now need configurations of api and state

---
+++ client/src/state/index.tsx
@@ -0,0 +1,12 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+export interface InitialStateTypes {
+    isSidebarCollapsed: boolean;
+    isDarkMode: boolean,
+}
+
+const initialState: InitialStateTypes = {
+    isSidebarCollapsed: false,
+    isDarkMode: false,
+};
